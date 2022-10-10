0eba0fba471049eb2418925640a738c6df54fde8
ymaru <dyun022@gmail.com>
2022-10-10 20:16:08 +0900

    fix: text

--- front-end/src/views/footer/Footer.vue
+++ front-end/src/views/footer/Footer.vue
@@ -24,14 +24,14 @@
       </div>
       <ul class="company_info_layout company_info">
         <p>
-          삼성 소프트웨어 청년 SW 아카데미
+          Movie DB
         </p>
-        <p>대표: SSAFY</p>
-        <p>이메일 주소 : donghe93@naver.com</p>
+        <p>대표: Movie DB</p>
+
         <p>
-          주소 : 대전광역시 유성구 동서대로 98-39
+          주소 : 대전광역시 유성구 지족동로
         </p>
-        <p>사업자 등록번호 : 11111-11111-11111</p>
+        <p>사업자 등록번호 : 12345-12331-12314</p>
       </ul>
     </div>
   </div>
@@ -39,63 +39,63 @@
 
 <script>
 export default {
-  name: 'Footer'
-}
+  name: "Footer",
+};
 </script>
 
 <style scoped>
-  .footer_box {
-    /* display: flex;
+.footer_box {
+  /* display: flex;
     justify-content: center;
     padding: 5px;
     background-color: #000000;
     color: #fff; */
-    background-color: black;
-    margin-top: -20px;
-  }
+  background-color: black;
+  margin-top: -20px;
+}
 
-  .box_layout {
-    display: flex;
-    flex-direction: column;
-    max-width: 980px;
-    margin: 20px auto 0;
-    padding: 0 4%;
-    color: rgb(255, 255, 255);
-  }
+.box_layout {
+  display: flex;
+  flex-direction: column;
+  max-width: 980px;
+  margin: 20px auto 0;
+  padding: 0 4%;
+  color: rgb(255, 255, 255);
+}
 
-  .box_font {
-    display: flex;
-    margin-bottom: 1em;
-  }
+.box_font {
+  display: flex;
+  margin-bottom: 1em;
+}
 
-  .box_icon {
-    height: 25px;
-    width: 36px;
-    color: gray;
-  }
+.box_icon {
+  height: 25px;
+  width: 36px;
+  color: gray;
+}
 
-  .box_iconsize {
-    font-size: 30px;
-  }
+.box_iconsize {
+  font-size: 30px;
+}
 
-  .info_layout {
-    display: flex;
-    justify-content: space-between;
-    margin: 0 0 14px 0;
-    font-size: 13px;
-  }
+.info_layout {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 0 14px 0;
+  font-size: 13px;
+}
 
-  .info_pst {
-    line-height: 3;
-  }  
+.info_pst {
+  line-height: 3;
+}
 
-  .company_info_layout {
-    display: flex;
-    flex-direction: column;
-  }
+.company_info_layout {
+  display: flex;
+  flex-direction: column;
+}
 
-  .company_info {
-    font-size: 11px;
-    line-height: 2;
-  }
+.company_info {
+  font-size: 11px;
+  line-height: 2;
+}
 </style>
